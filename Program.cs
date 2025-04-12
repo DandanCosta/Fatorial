0bb8defcd1a56944c35e27e516b3df91e5d07978
Danilo Costa <dandan_costa2222@outlook.com>
2025-04-12 15:44:10 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double n, fatorial, i ;
+
+Console.Write("Insira o numero para ser fatorado: ");
+n = double.Parse(Console.ReadLine()!);
+
+fatorial = n;
+
+if (n == 0){
+    fatorial = 1;
+    Console.WriteLine($"Fatorial de {n} é igual a {fatorial}");
+} 
+else if (n < 0){
+    Console.WriteLine("Este numro é negativo e não pode ssr fatorado");
+}
+else{
+    for (i = n - 1;i >= 1; i--){
+    fatorial = fatorial * i;
+    }
+    Console.WriteLine($"Fatorial de {n} é igual a {fatorial}");
+    }
+
+    
+
+
